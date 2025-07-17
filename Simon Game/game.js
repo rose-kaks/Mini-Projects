acfa27f796dccd197105ac98212a906eec82413d
rose-kaks <rosekakrania@gmail.com>
2025-07-17 23:09:59 +0530

    add keydown-level logic

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -1,10 +1,18 @@
-$("document").keypress(function() {
-    $("#level-title").text("Let's Begin!");
-});
+var start = 0;
 
 var gamePattern = [];
 var userClickedPattern = [];
 var buttonColors = ["red","blue","green","yellow"];
+
+$(document).keydown(function() {
+    if (start==0) {
+        $("#level-title").text("Let's Begin! Remember to <remember> the pattern! ;)");
+        setTimeout(function() {
+            $("#level-title").text("Get Ready for Level 1");
+            nextSequence();
+        }, 2000);
+    }
+});
 
 function playSound (name) {
     var audio = new Audio("./sounds/"+name+".mp3");
@@ -12,6 +20,8 @@ function playSound (name) {
 }
 
 function nextSequence() {
+    start++;
+    if (start!=1) $("#level-title").text("Congratulations! Now onto Level "+start);
     var randomNumber = Math.floor(Math.random() * 4); // Generates 0, 1, 2, or 3
     var randomChosenColor = buttonColors[randomNumber];
     $("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
@@ -26,7 +36,7 @@ $(".btn").click(function() {
     playSound(userChosenColor);
     animatePress(userChosenColor);
     userClickedPattern.push(userChosenColor);
-})
+});
 
 function animatePress(currColor) {
     $("#"+currColor).addClass("pressed");
