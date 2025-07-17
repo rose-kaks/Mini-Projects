8e7ec08090ca4ea79ed3cb87272ced5e6e486622
rose-kaks <rosekakrania@gmail.com>
2025-07-18 00:13:02 +0530

    logic to check and match

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -4,6 +4,7 @@ var gamePattern = [];
 var userClickedPattern = [];
 var buttonColors = ["red","blue","green","yellow"];
 
+// intial start
 $(document).keydown(function() {
     if (start==0) {
         $("#level-title").text("Let's Begin! Remember to <remember> the pattern! ;)");
@@ -14,12 +15,9 @@ $(document).keydown(function() {
     }
 });
 
-function playSound (name) {
-    var audio = new Audio("./sounds/"+name+".mp3");
-    audio.play();
-}
-
+//generate the sequence, level wise
 function nextSequence() {
+    userClickedPattern=[];
     start++;
     if (start!=1) $("#level-title").text("Congratulations! Now onto Level "+start);
     var randomNumber = Math.floor(Math.random() * 4); // Generates 0, 1, 2, or 3
@@ -30,14 +28,47 @@ function nextSequence() {
     gamePattern.push(randomChosenColor);
 }
 
+//input user's sequence at each level
 $(".btn").click(function() {
     var userChosenColor = $(this).attr("id");
     $(this).fadeOut(100).fadeIn(100);
     playSound(userChosenColor);
     animatePress(userChosenColor);
     userClickedPattern.push(userChosenColor);
+
+    checkAns(userClickedPattern.length-1);
 });
 
+//check for a match
+function checkAns(currLevel) {
+    if (gamePattern[currLevel]===userClickedPattern[currLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout (function() {
+                nextSequence();
+            },1000);
+        }
+    }
+    else {
+        var audio = new Audio ("./sounds/wrong.mp3");
+        audio.play();
+        $("body").addClass("game-over");
+
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("#level-title").text("Game Over :( Give it a Re-try? Press any Key!");
+
+        userClickedPattern=[]; start=0;
+        gamePattern=[];
+    }
+}
+
+//button effects
+function playSound (name) {
+    var audio = new Audio("./sounds/"+name+".mp3");
+    audio.play();
+}
 function animatePress(currColor) {
     $("#"+currColor).addClass("pressed");
     setTimeout(function() {
