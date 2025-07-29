de99efceedcb801a53405072c87a91e7aaf99cb9
rose-kaks <rosekakrania@gmail.com>
2025-07-29 23:49:46 +0530

    switched to ES

--- backend/NPM/index.js
+++ backend/NPM/index.js
@@ -1,2 +1,14 @@
-import { randomSuperhero } from "superheroes";
-const name=randomSuperhero();
+//this script is in CJS (Common JS) and thus we use 'require()'
+
+// var generateStupidName = require("sillyname");
+// var sillyname = generateStupidName();
+// console.log(`My name is ${sillyname}.`);
+
+//lets upgrade to import with ECMAScript (good practice now)
+//to use ES modules and import, we need to go to package.json and add or edit type, set it to "module"
+import generateStupidName from "sillyname";
+var sillyname = generateStupidName();
+console.log(`My name is ${sillyname}.`);
+
+// import { randomSuperhero } from "superheroes";
+// const name=randomSuperhero();
