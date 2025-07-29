7ac9e88ec6068ee48bec6726ecd57223a2225a67
rose-kaks <rosekakrania@gmail.com>
2025-07-30 01:22:50 +0530

    add qr-image and fs functionality

--- QR Code/index.js
+++ QR Code/index.js
@@ -1,5 +1,10 @@
 //1. to get user's url input
 import inquirer from 'inquirer';
+//2. to generate a qr code
+import qr from "qr-image";
+//3. write into a new file using fs
+import fs from "fs";
+
 
 inquirer
   .prompt([
@@ -12,7 +17,12 @@ inquirer
   .then((answers) => {
     // Use user feedback for... whatever!!
     const url = answers.URL;
-    console.log(url);
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+    fs.writeFile("URL.txt", url, (err)=>{
+        if (err) throw err;
+        console.log("URL has been saved!");
+    });
   })
   .catch((error) => {
     if (error.isTtyError) {
@@ -21,11 +31,3 @@ inquirer
       // Something else went wrong
     }
   });
-
-//2. to generate a qr code
-import qr from "qr-image";
- 
-var qr_svg = qr.image('I love QR!', { type: 'svg' });
-qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
- 
-var svg_string = qr.imageSync('I love QR!', { type: 'svg' });
