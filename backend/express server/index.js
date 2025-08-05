ba26f1da27ba72714fa74dd52a23fffc673d3238
rose-kaks <rosekakrania@gmail.com>
2025-08-05 16:39:28 +0530

    http get

--- backend/express server/index.js
+++ backend/express server/index.js
@@ -1,6 +1,13 @@
 import express from "express";
 const app = express();
 const port=3000;
+
+//get the homepage on load
+app.get("/", (req,res)=>{
+    res.send("<i>Hello, you beautiful person!</i>");
+});
+
+//start the server at given port
 app.listen(port, ()=> {
     console.log(`Server running on port ${port}.`);
-})
+});
