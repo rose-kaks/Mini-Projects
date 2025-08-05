ffbe973deb58fe23487882c5d14b95f4b6f79b7d
rose-kaks <rosekakrania@gmail.com>
2025-08-05 16:43:07 +0530

    handline endpoints

--- backend/express server/index.js
+++ backend/express server/index.js
@@ -7,6 +7,15 @@ app.get("/", (req,res)=>{
     res.send("<i>Hello, you beautiful person!</i>");
 });
 
+//handling endpoints other than just home
+app.get("/about", (req,res)=>{
+    res.send("Currently, you're meeting a <b>DeveLoPEr!</b>");
+});
+
+app.get("/contact", (req,res)=>{
+    res.send("Meet me at the Eastside!");
+});
+
 //start the server at given port
 app.listen(port, ()=> {
     console.log(`Server running on port ${port}.`);
